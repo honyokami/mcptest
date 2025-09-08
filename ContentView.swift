e996dd41244c876c7cc3802d1e020f66119c54f6
honyokami <meinvhuanhuanzi@gmail.com>
2025-09-08 11:43:56 +0800

    Create basic content view

---
+++ ContentView.swift
@@ -0,0 +1,17 @@
+//  ContentView.swift
+//  my4cuts
+//
+//  Created by AI Assistant on 2025/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to my4cuts!\n相机界面将在这里实现")
+            .font(.title)
+            .fontWeight(.bold)
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+}
