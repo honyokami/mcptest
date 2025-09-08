b93d63afcd044e4132e3ce70d714078ff74ff7c6
honyokami <meinvhuanhuanzi@gmail.com>
2025-09-08 11:42:11 +0800

    Create app entry file

---
+++ my4cutsApp.swift
@@ -0,0 +1,16 @@
+//  my4cutsApp.swift
+//  my4cuts
+//
+//  Created by AI Assistant on 2025/9/7.
+//
+
+import SwiftUI
+
+@main
+struct my4cutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
